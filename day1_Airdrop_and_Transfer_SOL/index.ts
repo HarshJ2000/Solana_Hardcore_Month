e4dd022004621565764edeb2b589cb2f7827c62e
HarshJ2000 <harsh976324@gmail.com>
2025-10-30 01:08:53 +0530

    Executed Transfer and confirmed it

--- day1_Airdrop_and_Transfer_SOL/index.ts
+++ day1_Airdrop_and_Transfer_SOL/index.ts
@@ -1,15 +1,41 @@
-import { Keypair, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import {
+  Keypair,
+  Connection,
+  LAMPORTS_PER_SOL,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
 
 const connection = new Connection("http://127.0.0.1:8899");
 
 async function main() {
-  const userAccount = new Keypair();
+  const senderAccount = new Keypair();
+  const receiverAccount = new Keypair();
   const airdropSol = await connection.requestAirdrop(
-    userAccount.publicKey,
+    senderAccount.publicKey,
     2 * LAMPORTS_PER_SOL
   );
   await connection.confirmTransaction(airdropSol);
 
+  const getSenderAccBal = await connection.getBalance(senderAccount.publicKey);
+  console.log(`Sender Account balance: ${getSenderAccBal}`);
+
+  const transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey: senderAccount.publicKey,
+      toPubkey: receiverAccount.publicKey,
+      lamports: 0.2 * LAMPORTS_PER_SOL,
+    })
+  );
+
+  const txnSignature = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [senderAccount]
+  );
+
+  console.log(`Transaction Complete.... -> ${txnSignature}`);
 }
 
 main();
