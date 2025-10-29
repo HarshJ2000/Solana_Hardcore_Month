be75212c44dc002c22d1a418f7d06d5d198a45e7
HarshJ2000 <harsh976324@gmail.com>
2025-10-30 00:23:59 +0530

    Added Connection, Created userAccount, Airdropped SOL

--- day1_Airdrop_and_Transfer_SOL/index.ts
+++ day1_Airdrop_and_Transfer_SOL/index.ts
@@ -1,3 +1,15 @@
-import lib = require("@solana/web3.js");
+import { Keypair, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
 
-const connection = new lib.Connection("http://127.0.0.1:8899");
+const connection = new Connection("http://127.0.0.1:8899");
+
+async function main() {
+  const userAccount = new Keypair();
+  const airdropSol = await connection.requestAirdrop(
+    userAccount.publicKey,
+    2 * LAMPORTS_PER_SOL
+  );
+  await connection.confirmTransaction(airdropSol);
+
+}
+
+main();
