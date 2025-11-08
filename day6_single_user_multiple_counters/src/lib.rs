4f3328cb66cba10ba4d54165c7118a5024341cb4
HarshJ2000 <harsh976324@gmail.com>
2025-11-08 23:47:14 +0530

    created basic counter program

--- day6_single_user_multiple_counters/src/lib.rs
+++ day6_single_user_multiple_counters/src/lib.rs
@@ -1,14 +1,76 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Counter {
+    owner: Pubkey,
+    count: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(BorshSerialize, BorshDeserialize)]
+enum InstructionType {
+    Initialize,
+    Increment(u32),
+    Decrement(u32),
+    Reset,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+entrypoint!(counter_program);
+pub fn counter_program(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let mut accounts_iter = accounts.iter();
+
+    let counter_account = next_account_info(&mut accounts_iter)?;
+    let signer = next_account_info(&mut accounts_iter)?;
+
+    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+
+    let mut counter_data = if counter_account.data_len() == 0 {
+        Counter {
+            owner: Pubkey::default(),
+            count: 0,
+        }
+    } else {
+        Counter::try_from_slice(&counter_account.data.borrow())?
+    };
+
+    match instruction_type {
+        InstructionType::Initialize => {
+            msg!("Initializing a counter......");
+            counter_data.owner = *signer.key;
+            counter_data.count = 0;
+            msg!("Counter was initialized by owner: {}", counter_data.owner);
+        }
+        InstructionType::Increment(value) => {
+            msg!("Executing Increment.....");
+            counter_data.count += value;
+        }
+        InstructionType::Decrement(value) => {
+            msg!("Executing Decrement......");
+            counter_data.count -= value;
+        }
+        InstructionType::Reset => {
+            msg!("Attempting to reset counter.....");
+            if counter_data.owner != *signer.key {
+                msg!("Unauthorized attempt to reset counter by : {}", signer.key);
+                return Err(solana_program::program_error::ProgramError::IllegalOwner);
+            }
+            counter_data.count = 0;
+            msg!("Counter reset by owner: {}", counter_data.owner);
+        }
     }
+
+    counter_data.serialize(&mut *counter_account.data.borrow_mut())?;
+    msg!("Counter updated: {}", counter_data.count);
+
+    Ok(())
 }
