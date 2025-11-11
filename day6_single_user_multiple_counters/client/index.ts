48694b355ccf074021b9d5479b4ffb2c2c88a99a
HarshJ2000 <harsh976324@gmail.com>
2025-11-11 14:00:12 +0530

    loading the programId and payer secrectkey from local solana

--- day6_single_user_multiple_counters/client/index.ts
+++ day6_single_user_multiple_counters/client/index.ts
@@ -1,3 +1,14 @@
-import { Connection } from "@solana/web3.js";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import fs from "fs";
 
-const connection = new Connection("http://127.0.0:8899");
+const PROGRAM_ID = new PublicKey(
+  "<replace this with the solana program ProgramID>"
+);
+
+const payer = Keypair.fromSecretKey(
+  Uint8Array.from(
+    JSON.parse(fs.readFileSync("~/.config/solana/id.json", "utf-8"))
+  )
+);
+
+const connection = new Connection("http://127.0.0.1:8899");
